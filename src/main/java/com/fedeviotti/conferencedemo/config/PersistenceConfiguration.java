057780bca4d1e36136c0246d8458a815bb968a1c
fedeviotti <fedeviotti@gmail.com>
2020-04-12 16:25:01 +0200

    readd bean persistence configuration

--- src/main/java/com/fedeviotti/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/fedeviotti/conferencedemo/config/PersistenceConfiguration.java
@@ -9,13 +9,13 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
 
-//    @Bean
-//    public DataSource dataSource() {
-//        DataSourceBuilder builder = DataSourceBuilder.create();
-//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-//        builder.username("postgres");
-//        builder.password("Welcome");
-//        return builder.build();
-//    }
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.username("postgres");
+        builder.password("Welcome");
+        return builder.build();
+    }
 
 }
